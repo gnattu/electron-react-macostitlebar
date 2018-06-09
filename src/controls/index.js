3b8ecf26c8ec9b71a57c6121df0febe9725a104d
gnattu <gnattuoc@me.com>
2018-06-09 14:45:30 +0800

    add default titleBar style

--- src/controls/index.js
+++ src/controls/index.js
@@ -1,41 +1,21 @@
-import React, { Component } from 'react'
-import { remote } from 'electron'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Close } from './close'
 import { Minimize } from './minimize'
 import { Fullscreen } from './fullscreen'
 
-class Controls extends Component {
-  static propTypes = {
-    disableMinimize: PropTypes.bool,
-    disableFullscreen: PropTypes.bool,
-    currentWindow: PropTypes.object,
-  }
-  static defaultProps = {
-    currentWindow: remote.getCurrentWindow(),
-  }
-  state = {
-    isFullscreen: this.props.currentWindow.isFullScreen(),
-    isFocused: this.props.currentWindow.isFocused(),
-  }
-  componentDidMount() {
-    this.props.currentWindow.addListener('enter-full-screen', () => this.setState({ isFullscreen: true }))
-    this.props.currentWindow.addListener('leave-full-screen', () => this.setState({ isFullscreen: false }))
-    this.props.currentWindow.addListener('focus', () => this.setState({ isFocused: true }))
-    this.props.currentWindow.addListener('blur', () => this.setState({ isFocused: false }))
-  }
-  render() {
-    const { disableMinimize, disableFullscreen, currentWindow} = this.props
-    const { isFullscreen, isFocused } = this.state
-    return (
-      !isFullscreen &&
-        <div className = 'macos-window-control'>
-          <Close currentWindow = {currentWindow} isFocused = {isFocused}></Close>
-          <Minimize disabled = {disableMinimize} currentWindow = {currentWindow} isFocused = {isFocused}></Minimize>
-          <Fullscreen disabled = {disableFullscreen} currentWindow = {currentWindow} isFocused={isFocused}></Fullscreen>
-        </div>
-    )
-  }
+export const Controls = ({ isFullscreen, isFocused, disableMinimize, disableFullscreen, currentWindow }) => (
+  !isFullscreen &&
+    <div className = 'macos-window-control'>
+      <Close currentWindow = {currentWindow} isFocused = {isFocused}></Close>
+      <Minimize disabled = {disableMinimize} currentWindow = {currentWindow} isFocused = {isFocused}></Minimize>
+      <Fullscreen disabled = {disableFullscreen} currentWindow = {currentWindow} isFocused={isFocused}></Fullscreen>
+    </div>
+)
+Controls.propTypes = {
+  isFullscreen: PropTypes.bool,
+  isFocused: PropTypes.bool,
+  disableMinimize: PropTypes.bool,
+  disableFullscreen: PropTypes.bool,
+  currentWindow: PropTypes.object.isRequired,
 }
-
-export { Controls }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { remote } from 'electron'
 
 import { Controls } from './controls/'
 
-export const macOSTitleBar = ({disableFullscreen, disableMinimize, currentWindow}) => (
-  <Controls disableFullscreen={disableFullscreen} disableMinimize={disableMinimize} currentWindow={currentWindow}></Controls>
-)
+class macOSTitleBar extends Component {
+  static propTypes = {
+    disableMinimize: PropTypes.bool,
+    disableMaximize: PropTypes.bool,
+    currentWindow: PropTypes.object,
+  }
+  static defaultProps = {
+    currentWindow: remote.getCurrentWindow(),
+  }
 
-macOSTitleBar.propTypes = {
-  disableMinimize: PropTypes.bool,
-  disableMaximize: PropTypes.bool,
-  currentWindow: PropTypes.object,
+  state = {
+    isFullscreen: this.props.currentWindow.isFullScreen(),
+    isFocused: this.props.currentWindow.isFocused(),
+  }
+
+  componentDidMount() {
+    this.props.currentWindow.addListener('enter-full-screen', () => this.setState({ isFullscreen: true }))
+    this.props.currentWindow.addListener('leave-full-screen', () => this.setState({ isFullscreen: false }))
+    this.props.currentWindow.addListener('focus', () => this.setState({ isFocused: true }))
+    this.props.currentWindow.addListener('blur', () => this.setState({ isFocused: false }))
+  }
+
+  render() {
+    const { disableFullscreen, disableMinimize, currentWindow } = this.props
+    const { isFullscreen, isFocused } = this.state
+    const classOnFocus = isFocused ? 'electron-macos-title-bar-focused' : ''
+    return (
+      !isFullscreen &&
+        <div className={`${classOnFocus} electron-macos-title-bar`}>
+          <Controls disableFullscreen={disableFullscreen} disableMinimize={disableMinimize} currentWindow={currentWindow} isFullscreen={isFullscreen} isFocused={isFocused}></Controls>
+        </div>
+    )
+  }
 }
+
+export { macOSTitleBar }
